326f8700cdbbf19a7dfbc211c33217da0879c061
Andreas Maertens <am@rcs.de>
2015-03-03 08:25:35 +0100

    vorüberlegungen auswerten

---
+++ programmierrunde/Fragebogen/auswerten.py
@@ -0,0 +1,10 @@
+"""
+
+() -> Prüfungsbogen laden -> FB
+PB -> Fragebogen laden -> FB
+FB, RB -> Auswertung erstellen -> Aw
+Aw -> Auswertung in Log schreiben -> ()
+Aw -> Auswertung ausgeben -> ()
+() -> Speicher reseten -> ()
+
+"""

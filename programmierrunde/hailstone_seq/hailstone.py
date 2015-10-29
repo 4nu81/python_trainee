7f303ec50ab02010bbb597248a83d624035a8218
4nu81 <am@rcs.de>
2015-10-29 13:57:49 +0100

    adds iterative hailstone

--- programmierrunde/hailstone_seq/hailstone.py
+++ programmierrunde/hailstone_seq/hailstone.py
@@ -1,14 +1,23 @@
 import sys
 
-def hailstone(number):
+def hailstoneRecursive(number):
     if number < 1:
         return []
     elif number == 1:
         return [1]
     elif number%2 == 1:
-        return [number] + hailstone(number * 3 + 1)
+        return [number] + hailstoneRecursive(number * 3 + 1)
     else:
-        return [number] + hailstone(number / 2)
+        return [number] + hailstoneRecursive(number / 2)
+
+def hailstone(number):
+    seq = []
+    if number >= 1:
+        seq.append(number)
+    while number > 1:
+        number = 3 * number + 1 if number & 1 else number//2
+        seq.append(number)
+    return seq
 
 if __name__ == '__main__':
     assert len(sys.argv) == 2
--- programmierrunde/hailstone_seq/test.py
+++ programmierrunde/hailstone_seq/test.py
@@ -3,16 +3,26 @@ import unittest
 
 class TestHailStone(unittest.TestCase):
     def test_1(self):
-        assert hailstone(1) == [1], "False result with 1"
+        assert hailstone(1) == [1], "False result with 1. Expected [1]. Result %s" % str(h)
 
     def test_7(self):
-        assert hailstone(7) == [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1], "False result with 7"
+        assert hailstone(7) == [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1], "False result with 7. Expected [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]. Result %s" % str(h)
 
     def test_0(self):
-        assert hailstone(0) == [], "False result with 0"
+        h = hailstone(0)
+        assert h == [], "False result with 0. Expected []. Result %s" % str(h)
 
     def test_1_neg(self):
-        assert hailstone(-1) == [], "False result with -1"
+        assert hailstone(-1) == [], "False result with -1. Expected []. Result %s" % str(h)
+
+    def test_27(self):
+        h = hailstone(27)
+        # element count
+        assert len(h)==112
+        # first 4 elements
+        assert h[:4]==[27, 82, 41, 124]
+        # last 4 elements
+        assert h[-4:]==[8, 4, 2, 1]
 
 if __name__ == "__main__":
     unittest.main()
